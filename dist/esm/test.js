5e43c952c42a3e1fb9d57e9fd02794a021bda34d
Simon Gander <simon_gander@nextlvlup.com>
2023-01-29 19:11:54 +0100

    update file structure

--- dist/esm/test.js
+++
@@ -1,8 +0,0 @@
-import { Socket } from "net";
-import { UBXParser } from ".";
-var parser = new UBXParser();
-var client = new Socket();
-client.connect({ host: "192.168.1.154", port: 1234 }, function () { return console.log("connected"); });
-client.on("data", function (buffer) { return parser.parse(buffer); });
-parser.on("error", function (error) { return console.error(error); });
-parser.on("data", function (data) { return console.log(data); });
