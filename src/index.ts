39463ceebbfdee4088a0d1fcf590c70abbba74ad
Simon Gander <simon_gander@nextlvlup.com>
2023-02-04 09:55:52 +0100

    add documentation

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import { UBXParser } from "./UBXParser";
 import { PacketParser } from "./PacketParser";
+import { readBitFromUInt8, readBitFromUInt16, readBitFromUInt32 } from "./Utils";
 
-export { UBXParser, PacketParser };
+export { UBXParser, PacketParser, readBitFromUInt8, readBitFromUInt16, readBitFromUInt32 };
