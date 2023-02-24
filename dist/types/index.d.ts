5de306741468fed38c005f30c5df6e0b2d271803
Simon Gander <simon_gander@nextlvlup.com>
2023-02-24 15:45:52 +0100

    export packet types

--- dist/types/index.d.ts
+++ dist/types/index.d.ts
@@ -1,4 +1,7 @@
 import { UBXParser } from "./UBXParser";
 import { PacketParser } from "./PacketParser";
 import { readBitFromUInt8, readBitFromUInt16, readBitFromUInt32 } from "./Utils";
-export { UBXParser, PacketParser, readBitFromUInt8, readBitFromUInt16, readBitFromUInt32 };
+import { UBX_ESF_MEAS_DATA } from "./parser/ubx-esf-meas";
+import { UBX_ESF_STATUS_DATA } from "./parser/ubx-esf-status";
+import { UBX_NAV_PVT_DATA } from "./parser/ubx-nav-pvt";
+export { UBXParser, PacketParser, readBitFromUInt8, readBitFromUInt16, readBitFromUInt32, UBX_ESF_MEAS_DATA, UBX_ESF_STATUS_DATA, UBX_NAV_PVT_DATA };
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,8 @@
 import { UBXParser } from "./UBXParser";
 import { PacketParser } from "./PacketParser";
 import { readBitFromUInt8, readBitFromUInt16, readBitFromUInt32 } from "./Utils";
+import { UBX_ESF_MEAS_DATA } from "./parser/ubx-esf-meas";
+import { UBX_ESF_STATUS_DATA } from "./parser/ubx-esf-status";
+import { UBX_NAV_PVT_DATA } from "./parser/ubx-nav-pvt";
 
-export { UBXParser, PacketParser, readBitFromUInt8, readBitFromUInt16, readBitFromUInt32 };
+export { UBXParser, PacketParser, readBitFromUInt8, readBitFromUInt16, readBitFromUInt32, UBX_ESF_MEAS_DATA, UBX_ESF_STATUS_DATA, UBX_NAV_PVT_DATA };
