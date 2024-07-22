e1552335eb69704b1744f0756b9aaa71084f4135
Bill Glase <bglase@icloud.com>
2024-07-21 22:07:20 -0400

    Update export of new types

--- dist/types/index.d.ts
+++ dist/types/index.d.ts
@@ -4,11 +4,11 @@ import { readBitFromUInt8, readBitFromUInt16, readBitFromUInt32 } from "./Utils"
 import { UBX_ESF_MEAS_DATA } from "./parser/ubx-esf-meas";
 import { UBX_ESF_STATUS_DATA } from "./parser/ubx-esf-status";
 import { UBX_NAV_PVT_DATA } from "./parser/ubx-nav-pvt";
-import { UBX_NAV_SAT_Parser } from "./parser/ubx-nav-sat";
+import { UBX_NAV_SAT_Parser, UBX_NAV_SAT_PAYLOAD, UBX_NAV_SAT_SV } from "./parser/ubx-nav-sat";
 import { UBX_NAV_PVT_Parser } from "./parser/ubx-nav-pvt";
 import { UBX_MON_VER_Parser } from "./parser/ubx-mon-ver";
 import { UBX_ESF_STATUS_Parser } from "./parser/ubx-esf-status";
 import { UBX_ESF_MEAS_Parser } from "./parser/ubx-esf-meas";
 export { UBXParser, PacketParser, readBitFromUInt8, readBitFromUInt16, readBitFromUInt32, UBX_ESF_MEAS_DATA, UBX_ESF_STATUS_DATA, UBX_NAV_PVT_DATA };
-export { UBX_NAV_SAT_Parser, UBX_MON_VER_Parser, UBX_NAV_PVT_Parser, UBX_ESF_STATUS_Parser, UBX_ESF_MEAS_Parser };
+export { UBX_NAV_SAT_Parser, UBX_NAV_SAT_PAYLOAD, UBX_NAV_SAT_SV, UBX_MON_VER_Parser, UBX_NAV_PVT_Parser, UBX_ESF_STATUS_Parser, UBX_ESF_MEAS_Parser };
 export * from "./types";
--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ import { UBX_ESF_MEAS_DATA } from "./parser/ubx-esf-meas";
 import { UBX_ESF_STATUS_DATA } from "./parser/ubx-esf-status";
 import { UBX_NAV_PVT_DATA } from "./parser/ubx-nav-pvt";
 
-import { UBX_NAV_SAT_Parser } from "./parser/ubx-nav-sat";
+import { UBX_NAV_SAT_Parser, UBX_NAV_SAT_PAYLOAD, UBX_NAV_SAT_SV } from "./parser/ubx-nav-sat";
 import { UBX_NAV_PVT_Parser } from "./parser/ubx-nav-pvt";
 
 import { UBX_MON_VER_Parser } from "./parser/ubx-mon-ver";
@@ -17,7 +17,7 @@ import { UBX_ESF_MEAS_Parser } from "./parser/ubx-esf-meas";
 
 export { UBXParser, PacketParser, readBitFromUInt8, readBitFromUInt16, readBitFromUInt32, UBX_ESF_MEAS_DATA, UBX_ESF_STATUS_DATA, UBX_NAV_PVT_DATA };
 
-export { UBX_NAV_SAT_Parser, UBX_MON_VER_Parser, UBX_NAV_PVT_Parser, UBX_ESF_STATUS_Parser, UBX_ESF_MEAS_Parser };
+export { UBX_NAV_SAT_Parser, UBX_NAV_SAT_PAYLOAD, UBX_NAV_SAT_SV, UBX_MON_VER_Parser, UBX_NAV_PVT_Parser, UBX_ESF_STATUS_Parser, UBX_ESF_MEAS_Parser };
 
 
 export * from "./types";
